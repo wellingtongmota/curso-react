d9aa0d2071f5f0e2de5d6e0821aa01b7d6bc2c35
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-02-24 08:40:48 -0300

    Aula 9 - Eventos no React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import './App.css';
-import HelloWorld from './componets/HelloWorld'
-import Pessoa from './componets/Pessoa';
-import SayMyName from './componets/SayMyName';
-import Frase from './componets/Frase';
-import List from './componets/List';
+// import HelloWorld from './componets/HelloWorld'
+// import Pessoa from './componets/Pessoa';
+// import SayMyName from './componets/SayMyName';
+// import Frase from './componets/Frase';
+// import List from './componets/List';
+import Evento from './componets/Evento';
+import Form from './componets/Form';
 
 function App() {
-  const nome = 'Maria'
+  // const nome = 'Maria'
 
   return (
     <div className="App">
-      {/* <HelloWorld /> */}
+      {/* <HelloWorld />
       <Frase />
       <Frase />
       <SayMyName nome='Wellington'/>
       <SayMyName nome='João' />
       <SayMyName nome={nome} />
       <Pessoa nome='Rodrigo' idade='28' profissao='Programador' foto='https://via.placeholder.com/150'/>
-      <List />
+      <List /> */}
+
+      <h1>Testando eventos</h1>
+      <Evento />
+      <Form />
+
     </div>
   );
 }
---
+++ src/componets/Evento.js
@@ -0,0 +1,13 @@
+const Evento = () => {
+
+  const meuEvento = () => console.log('Opa, fui ativado!')
+
+  return (
+    <div>
+      <p>Clique para disparar um evento: </p>
+      <button onClick={meuEvento}>Ativar</button>
+    </div>
+  )
+}
+
+export default Evento
