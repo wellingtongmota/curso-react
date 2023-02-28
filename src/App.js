964d2195b301e0269b2ae8fba5728aa3055e2b5f
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-02-28 10:03:47 -0300

    Aula 12 - Renderização condicional if

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
-import Evento from './componets/Evento';
-import Form from './componets/Form';
+import Condicional from './componets/Condicional';
 
 function App() {
   return (
     <div className="App">
-      <h1>Testando eventos</h1>
-      <Evento />
-      <Form />
-
+      <h1>Renderização condicional</h1>
+      <Condicional />
     </div>
   );
 }
---
+++ src/componets/Condicional.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const Condicional = () => {
+
+  const [email, setEmail] = useState()
+  const [userEmail, setUserEmail] = useState()
+
+  const enviarEmail = (e) => {
+    e.preventDefault()
+    setUserEmail(email)
+  }
+
+  const limparEmail = (e) => {
+    e.preventDefault()
+    setUserEmail('')
+  }
+
+  return (
+    <div>
+      <h2>Cadastre seu e-mail:</h2>
+      <form>
+        <input type="email" placeholder="Digite seu e-mail..." onChange={(e) => setEmail(e.target.value)}/>
+        <button type="submit" onClick={enviarEmail}>Enviar e-mail</button>
+        
+        {userEmail && (
+          <div>
+            <p>O e-mail do usuário é: {userEmail}</p>
+            <button onClick={limparEmail}>Limpar e-mail</button>
+          </div>
+        )}
+
+      </form>
+    </div>
+  )
+}
+
+export default Condicional
