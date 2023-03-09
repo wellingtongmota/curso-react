4728f548f382e86a6dc0f4742257ccf14a3b02cd
Wellington Galvão <wellingtongalvao96@gmail.com>
2023-03-09 08:28:23 -0300

    Aula 13 - Renderização de listas

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import './App.css';
-import Condicional from './componets/Condicional';
+import OutraLista from './componets/OutraLista';
 
 function App() {
+
+  const meusItens = ['React', 'Vue', 'Angular']
+
   return (
     <div className="App">
-      <h1>Renderização condicional</h1>
-      <Condicional />
+      <h1>Renderização de listas</h1>
+      <OutraLista itens={meusItens} />
+      <OutraLista itens={[]}/>
     </div>
   );
 }
